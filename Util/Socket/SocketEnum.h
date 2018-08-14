1c086e053d963a85fc2cbed439bd12aa8e67f0db
eomtaewook <trim0917@gmail.com>
2018-08-14 12:59:11 +0900

    .

--- Util/Socket/SocketEnum.h
+++ Util/Socket/SocketEnum.h
@@ -1,13 +1,6 @@
 #pragma once
 #include "NS.h"
 NS_SOCKET_BEGIN
-enum class VertifyResult
-{
-	Vertify_Accept = 0,
-	Vertify_Reject,
-	Vertify_Ignore,
-	Vertify_Forward
-};
 enum class Mode
 {
 	Receive = 0,
