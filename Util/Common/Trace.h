80ed7f03d4c08f346090ced71235e46a9992f6cb
eomtaewook <trim0917@gmail.com>
2018-09-17 09:29:46 +0900

    .

--- Util/Common/Trace.h
+++ Util/Common/Trace.h
@@ -2,14 +2,14 @@
 #include "NS.h"
 #include <string>
 NS_COMMON_BEGIN
-#define Assert_1(condition) Trace::_Assert(__LINE__, __FILE__, condition)
-#define Assert_2(condition,message) Trace::_Assert(__LINE__, __FILE__, condition, message)
+class Trace
+{
+public:
+#define Assert_1(condition) _Assert(__LINE__, __FILE__, condition)
+#define Assert_2(condition,message) _Assert(__LINE__, __FILE__, condition, message)
 
 #define _GET_OVERRIDE(_1, _2, NAME, ...) NAME
 #define Assert(...) _GET_OVERRIDE(__VA_ARGS__, Assert_2, Assert_1)(__VA_ARGS__)
-
-class Trace
-{
 private:
 	static bool _Debug;
 public:
@@ -17,7 +17,7 @@ public:
 	static void WriteLine(const std::string& message);
 	static void Write(const std::string& message, const std::string& category);
 	static void WriteLine(const std::string& message, const std::string& category);
-public:
+private:
 	static void _Assert(const int& line, const std::string& file, const bool& condition);
 	static void _Assert(const int& line, const std::string& file, const bool& condition, const std::string& message);
 };
--- Util/Socket/IOCPSocketClient.cpp
+++
@@ -1,7 +0,0 @@
-#include "IOCPSocketClient.h"
-
-NS_SOCKET_BEGIN
-
-
-
-NS_SOCKET_END
